7d36c0c82066d7fefe07e78bb0ba161fa259a65e
Andreas Linde <mail@andreaslinde.de>
2010-11-25 19:07:25 +0100

    - Updated README with list of features - Updated build script for upload automatisation - Added predefined .htaccess for stats page security (add your user and password to .htpasswd with htpasswd in the Terminal) - Added predefined .htacces for include directory, if it is not moved out of Apache htdocs

--- client/Beta Automatisation/Beta Automatisation.sh
+++ client/Beta Automatisation/Beta Automatisation.sh
@@ -1,114 +1,111 @@
 #!/bin/sh
 
 # This has to match the name of the configuration you use for Ad-Hoc builds!
-adHocConfigurationName="Ad-Hoc";
+adHocConfigurationName="Ad Hoc"
+adHocCertificate="iPhone Distribution: Your Name Here"
+provisioningProfilePath="/Users/yourname/Library/MobileDevice/Provisioning Profiles/profile.mobileprovision"
 
-executableName="My Application";
-bundleDisplayName="My Application";
+executableName="Your Application"
+bundleDisplayName="Your Application"
 
-symbolicationDirectoryPath="${PROJECT_DIR}/Development/CrashReporter/Symbolication";
+archiveDirectoryPath="${PROJECT_DIR}/Releases/Beta";
 dropboxDistributionDirectoryPath="/users/myusername/Dropbox/My Application Beta";
 
-ftpUsername="username";
-ftpPassword="password";
-ftpServerDirectoryPath="my-cool-server.com/httpdocs/beta";
+uploadMethod="scp"
 
+ftpUsername="username"
+ftpPassword="password"
+ftpServerDirectoryPath="my-cool-server.com/httpdocs/beta"
 
+scpUser="username"
+scpHost="yourhost.com"
+scpPath="/path/to/app"
 
-if ([ "${EFFECTIVE_PLATFORM_NAME}" == "-iphoneos" ])
+applicationPath="${CONFIGURATION_BUILD_DIR}/$executableName.app"
+
+if [[ "${EFFECTIVE_PLATFORM_NAME}" == "-iphoneos" && "${BUILD_STYLE}" == $adHocConfigurationName && -e $applicationPath ]]
 then
-	
-	applicationPath="${CONFIGURATION_BUILD_DIR}/$executableName.app";
-	bundleVersion=$(defaults read "$applicationPath/Info" CFBundleVersion);
-	bundleIdentifier=$(defaults read "$applicationPath/Info" CFBundleIdentifier);
-	
-	# Move .app file to a secure location
-  	if !([ -e "$symbolicationDirectoryPath/$bundleIdentifier $bundleVersion.app" ])
-     then
+    bundleVersion=$(defaults read "$applicationPath/Info" CFBundleVersion)
+    bundleIdentifier=$(defaults read "$applicationPath/Info" CFBundleIdentifier)
+    
+    # Zip the app and dSYM
+    zipPath="${CONFIGURATION_BUILD_DIR}/$executableName $bundleVersion.zip"
+    
+    zip -qr "${CONFIGURATION_BUILD_DIR}/$executableName $bundleVersion.zip" . -i ${CONFIGURATION_BUILD_DIR}/$executableName.app ${CONFIGURATION_BUILD_DIR}/$executableName.app.dSYM
+    
+    # Move new archive
+    if !([ -e "$archiveDirectoryPath/$bundleIdentifier $bundleVersion.zip" ])
+    then
+        mv "$zipPath" "$archiveDirectoryPath";
+    fi
+    
+    cd "${CONFIGURATION_BUILD_DIR}"
 
-       	cp -r $applicationPath $symbolicationDirectoryPath;
-		mv "$symbolicationDirectoryPath/$executableName.app" "$symbolicationDirectoryPath/$bundleIdentifier $bundleVersion.app";
-		
-     fi
+    # Archive the application for Ad-Hoc distribution
+    /usr/bin/xcrun -sdk iphoneos PackageApplication "$executableName.app" -o "${CONFIGURATION_BUILD_DIR}/$executableName.ipa" --sign "$adHocCertificate" --embed "$provisioningProfilePath"
+    
+    # Generate .plist file for wireless app distribution
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+         <!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+         <plist version=\"1.0\">
+         <dict>
+            <key>items</key>
+            <array>
+                <dict>
+                    <key>assets</key>
+                    <array>
+                        <dict>
+                            <key>kind</key>
+                            <string>software-package</string>
+                            <key>url</key>
+                            <string>__URL__</string>
+                        </dict>
+                    </array>
+                    <key>metadata</key>
+                    <dict>
+                        <key>bundle-identifier</key>
+                        <string>$bundleIdentifier</string>
+                        <key>bundle-version</key>
+                        <string>$bundleVersion</string>
+                        <key>kind</key>
+                        <string>software</string>
+                        <key>title</key>
+                        <string>$bundleDisplayName</string>
+                    </dict>
+                </dict>
+            </array>
+         </dict>
+         </plist>" > "${CONFIGURATION_BUILD_DIR}/$executableName.plist";
+    
+    # Move the archived application to Dropbox
+    cp "$executableName.ipa" "$dropboxDistributionDirectoryPath/$executableName.ipa";
+    
+    # Open Terminal and upload the .ipa and .plist files
+    if ([ "$uploadMethod" == "ftp" ])
+    then
+        echo "Uploading $executableName.ipa and $executableName.plist to $ftpServerDirectoryPath...";
+        
+        /usr/bin/osascript <<-EOF           
+on isApplicationRunning(application)
+tell application "System Events" to (name of processes) contains application
+end isApplicationRunning
 
-	# Move .app.dSYM file to a secure location
-	if !([ -e "$symbolicationDirectoryPath/$bundleIdentifier $bundleVersion.app.dSYM" ])
-  	   then
+if not (isApplicationRunning("Terminal")) then
+tell application "Terminal"
+    launch
+    close window 1
+end tell
+end if
 
-       	cp -r "$applicationPath.dSYM" $symbolicationDirectoryPath;
-		mv "$symbolicationDirectoryPath/$executableName.app.dSYM" "$symbolicationDirectoryPath/$bundleIdentifier $bundleVersion.app.dSYM";
-		
-     fi
-
-
-	if ([ "${BUILD_STYLE}" == $adHocConfigurationName ])
-	then
-		cd "${CONFIGURATION_BUILD_DIR}";
-
-		# Archive the application for Ad-Hoc distribution
-		mkdir "${CONFIGURATION_BUILD_DIR}/Payload";
-		cp -r $applicationPath "${CONFIGURATION_BUILD_DIR}/Payload";
-		(zip -qr "$executableName.ipa" Payload);
-		rm -rf "${CONFIGURATION_BUILD_DIR}/Payload";
-		
-		# Generate .plist file for wireless app distribution
-		echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-		 	 <!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
-			 <plist version=\"1.0\">
-			 <dict>
-				<key>items</key>
-				<array>
-					<dict>
-						<key>assets</key>
-						<array>
-							<dict>
-								<key>kind</key>
-								<string>software-package</string>
-								<key>url</key>
-								<string>__URL__</string>
-							</dict>
-						</array>
-						<key>metadata</key>
-						<dict>
-							<key>bundle-identifier</key>
-							<string>$bundleIdentifier</string>
-							<key>bundle-version</key>
-							<string>$bundleVersion</string>
-							<key>kind</key>
-							<string>software</string>
-							<key>title</key>
-							<string>$bundleDisplayName</string>
-						</dict>
-					</dict>
-				</array>
-			 </dict>
-			 </plist>" > "${CONFIGURATION_BUILD_DIR}/$executableName.plist";
-		
-		# Move the archived application to Dropbox
-		cp "$executableName.ipa" "$dropboxDistributionDirectoryPath/$executableName.ipa";
-
-		# Open Terminal and upload the .ipa and .plist files
-		echo "Uploading $executableName.ipa and $executableName.plist to $ftpServerDirectoryPath...";
-		/usr/bin/osascript <<-EOF			
-			on isApplicationRunning(application)
-				tell application "System Events" to (name of processes) contains application
-			end isApplicationRunning
-
-			if not (isApplicationRunning("Terminal")) then
-				tell application "Terminal"
-					launch
-					close window 1
-				end tell
-			end if
-
-			tell application "Terminal"
-				do script with command "curl -T \"${CONFIGURATION_BUILD_DIR}/$executableName.ipa\" \"ftp://$ftpUsername:$ftpPassword@$ftpServerDirectoryPath/$bundleIdentifier/$executableName.ipa\""
-				do script with command "curl -T \"${CONFIGURATION_BUILD_DIR}/$executableName.plist\" \"ftp://$ftpUsername:$ftpPassword@$ftpServerDirectoryPath/$bundleIdentifier/$executableName.plist\""
-			end tell
-		EOF
-
-		cd "${PROJECT_DIR}";
-	fi
+tell application "Terminal"
+do script with command "curl -T \"${CONFIGURATION_BUILD_DIR}/$executableName.ipa\" \"ftp://$ftpUsername:$ftpPassword@$ftpServerDirectoryPath/$bundleIdentifier/$executableName.ipa\""
+do script with command "curl -T \"${CONFIGURATION_BUILD_DIR}/$executableName.plist\" \"ftp://$ftpUsername:$ftpPassword@$ftpServerDirectoryPath/$bundleIdentifier/$executableName.plist\""
+end tell
+EOF
+    elif [[ "$uploadMethod" == "scp" ]]
+    then
+        echo "Uploading $executableName.ipa and $executableName.plist to $scpHost...";
+        scp "${CONFIGURATION_BUILD_DIR}/$executableName.ipa" $scpUser@$scpHost:"$scpPath/$bundleIdentifier"
+        scp "${CONFIGURATION_BUILD_DIR}/$executableName.plist" $scpUser@$scpHost:"$scpPath/$bundleIdentifier"
+    fi
 fi
-
-exit 0;
