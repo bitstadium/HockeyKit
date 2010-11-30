77dd0b167859245b07b728451a234b949ad5e81e
Andreas Linde <mail@andreaslinde.de>
2010-12-01 05:37:38 +0800

    Adjust stats page to handle CFBundleShortVersionString too and fix the bookmarklet, which wasn't working

--- server/php/public/stats/index.php
+++ server/php/public/stats/index.php
@@ -30,7 +30,11 @@
                 </div>
                 <div class="column span-8">
                     <h2><?php echo $app[iOSUpdater::INDEX_APP] ?></h2>
+                  <?php if ($app[iOSUpdater::INDEX_SUBTITLE]) { ?>
+                    <p><b>Version:</b> <?php echo $app[iOSUpdater::INDEX_SUBTITLE] ?> (<?php echo $app[iOSUpdater::INDEX_VERSION] ?>)</p>
+                  <?php } else { ?>
                     <p><b>Version:</b> <?php echo $app[iOSUpdater::INDEX_VERSION] ?></p>
+                  <?php } ?>
                     <p><b>Released:</b> <?php echo date('m/d/Y H:i:s', $app[iOSUpdater::INDEX_DATE]) ?></p>
 
                 <?php if ($app[iOSUpdater::INDEX_NOTES]) : ?>
@@ -68,7 +72,7 @@
             ?>
 
               <hr/>
-              <p>Drag this bookmarklet <a href='javascript:(function(){var expIDs = /<td class="id" title="([^"]+)/g;var elIDs= document.body.innerHTML.match(expIDs);var expNames=/<td class="name">.*?<span([^<]+)/g;var elNames=document.body.innerHTML.match(expNames);var content = "";for (var i=0; i<elIDs.length; i++){var id=elIDs[i].substr(elIDs[i].lastIndexOf("79ba73aa62ee2bcf89cf1f88266ea89008565ef5quot;")+1);var name="";if (elNames[i].search("<span title=")>-1){var start=elNames[i].lastIndexOf("title=")+7;var amount=elNames[i].lastIndexOf(">")-1-start;name=elNames[i].substr(start,amount);}else{name=elNames[i].substr(elNames[i].lastIndexOf(">")+1);}content+=id+";"+name+"\n";}document.body.innerHTML=content;alert("Copy the content and paste it into userlist.txt in your Hockey stats directory")}());'>Hockey iOS Portal Device Parser</a> into your bookmarks bar. Then open the <a href="https://developer.apple.com/ios/manage/devices/index.action">iOS Provisioning Portal</a>, invoke the bookmarklet and paste the resulting new content into the userlist.txt inside this stats directory.</p>
+              <p>Drag this bookmarklet <a href='javascript:(function(){var%20expIDs%20=%20/<td%20class="id"%20title="([^"]+)/g;var%20elIDs=%20document.body.innerHTML.match(expIDs);var%20expNames=/<td%20class="name">.*?<span([^<]+)/g;var%20elNames=document.body.innerHTML.match(expNames);var%20content%20=%20"";for%20(var%20i=0;%20i<elIDs.length;%20i++){var%20id=elIDs[i].substr(elIDs[i].lastIndexOf("\"")+1);var%20name="";if%20(elNames[i].search("<span%20title=")>-1){var%20start=elNames[i].lastIndexOf("title=")+7;var%20amount=elNames[i].lastIndexOf(">")-1-start;name=elNames[i].substr(start,amount);}else{name=elNames[i].substr(elNames[i].lastIndexOf(">")+1);}content+=id+";"+name+"\n";}document.body.innerHTML=content;alert("Copy%20the%20content%20and%20paste%20it%20into%20userlist.txt%20in%20your%20Hockey%20stats%20directory")}());'>Hockey iOS Portal Device Parser</a> into your bookmarks bar. Then open the <a href="https://developer.apple.com/ios/manage/devices/index.action">iOS Provisioning Portal</a>, invoke the bookmarklet and paste the resulting new content into the userlist.txt inside this stats directory.</p>
 
             </div>
         </div>
