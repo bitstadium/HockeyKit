0ea1774aabe70b46346f7fea8dc82128b08cd203
Peter Steinberger <me@petersteinberger.com>
2011-01-09 21:08:16 +0100

    update project for new helper files initial add of PSAppStoreHeader

---
+++ client/Helper/PSAppStoreHeader.h
@@ -0,0 +1,13 @@
+//
+//  PSAppStoreHeader.h
+//  HockeyDemo
+//
+//  Created by Peter Steinberger on 09.01.11.
+//  Copyright 2011 Peter Steinberger. All rights reserved.
+//
+
+@interface PSAppStoreHeader : UIView {
+
+}
+
+@end
---
+++ client/Helper/PSAppStoreHeader.m
@@ -0,0 +1,21 @@
+//
+//  PSAppStoreHeader.m
+//  HockeyDemo
+//
+//  Created by Peter Steinberger on 09.01.11.
+//  Copyright 2011 Peter Steinberger. All rights reserved.
+//
+
+#import "PSAppStoreHeader.h"
+
+@implementation PSAppStoreHeader
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark NSObject
+
+- (void)drawRect:(CGRect)rect {
+
+}
+
+@end
