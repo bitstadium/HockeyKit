dc04857f1e31d17f4d7b09424c3eaf855af42503
Andreas Linde <mail@andreaslinde.de>
2011-01-22 02:19:01 +0100

    Adjust stats page to new class naming

--- server/php/public/stats/index.php
+++ server/php/public/stats/index.php
@@ -2,14 +2,14 @@
     require_once('../config.php');
     require('../' . constant('HOCKEY_INCLUDE_DIR'));
     
-    $ios = new iOSUpdater(dirname(__FILE__).DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR);
+    $apps = new AppUpdater(dirname(__FILE__).DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR);
     $baseURL = "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>iOS App Installer Statistics</title>
+        <title>App Installer Statistics</title>
     	<meta name="viewport" content="width=device-width" />
         <link rel="stylesheet" href="../blueprint/screen.css" type="text/css" media="screen, projection">
         <link rel="stylesheet" href="../blueprint/print.css" type="text/css" media="print">
@@ -23,24 +23,24 @@
                 <h1>Install Apps Statistics</h1>
 
             <?php 
-                foreach ($ios->applications as $i => $app) :
+                foreach ($apps->applications as $i => $app) :
             ?>
                 <div class="column span-3">
-                <?php if ($app[iOSUpdater::INDEX_IMAGE]) { ?>
-                    <img class="icon" src="../<?php echo $app[iOSUpdater::INDEX_IMAGE] ?>">
+                <?php if ($app[AppUpdater::INDEX_IMAGE]) { ?>
+                    <img class="icon" src="../<?php echo $app[AppUpdater::INDEX_IMAGE] ?>">
                 <?php } ?>
                 </div>
                 <div class="column span-8">
-                    <h2><?php echo $app[iOSUpdater::INDEX_APP] ?></h2>
-                  <?php if ($app[iOSUpdater::INDEX_SUBTITLE]) { ?>
-                    <p><b>Version:</b> <?php echo $app[iOSUpdater::INDEX_SUBTITLE] ?> (<?php echo $app[iOSUpdater::INDEX_VERSION] ?>)</p>
+                    <h2><?php echo $app[AppUpdater::INDEX_APP] ?></h2>
+                  <?php if ($app[AppUpdater::INDEX_SUBTITLE]) { ?>
+                    <p><b>Version:</b> <?php echo $app[AppUpdater::INDEX_SUBTITLE] ?> (<?php echo $app[AppUpdater::INDEX_VERSION] ?>)</p>
                   <?php } else { ?>
-                    <p><b>Version:</b> <?php echo $app[iOSUpdater::INDEX_VERSION] ?></p>
+                    <p><b>Version:</b> <?php echo $app[AppUpdater::INDEX_VERSION] ?></p>
                   <?php } ?>
-                    <p><b>Released:</b> <?php echo date('m/d/Y H:i:s', $app[iOSUpdater::INDEX_DATE]) ?></p>
+                    <p><b>Released:</b> <?php echo date('m/d/Y H:i:s', $app[AppUpdater::INDEX_DATE]) ?></p>
 
-                <?php if ($app[iOSUpdater::INDEX_NOTES]) : ?>
-                    <p><b>What's New:</b><br/><?php echo $app[iOSUpdater::INDEX_NOTES] ?></p>
+                <?php if ($app[AppUpdater::INDEX_NOTES]) : ?>
+                    <p><b>What's New:</b><br/><?php echo $app[AppUpdater::INDEX_NOTES] ?></p>
                 <?php endif ?>
 
                 </div>
@@ -55,13 +55,13 @@
                             <th>Last Check</th>
                         </tr>
             <?php 
-                foreach ($app[iOSUpdater::INDEX_STATS] as $i => $device) :
+                foreach ($app[AppUpdater::INDEX_STATS] as $i => $device) :
                     echo "<tr>";
-                    echo "  <td>".$device[iOSUpdater::DEVICE_APPVERSION]."</td>";
-                    echo "  <td>".$device[iOSUpdater::DEVICE_USER]."</td>";
-                    echo "  <td>".$device[iOSUpdater::DEVICE_OSVERSION]."</td>";
-                    echo "  <td>".$device[iOSUpdater::DEVICE_PLATFORM]."</td>";
-                    echo "  <td>".$device[iOSUpdater::DEVICE_LASTCHECK]."</td>";
+                    echo "  <td>".$device[AppUpdater::DEVICE_APPVERSION]."</td>";
+                    echo "  <td>".$device[AppUpdater::DEVICE_USER]."</td>";
+                    echo "  <td>".$device[AppUpdater::DEVICE_OSVERSION]."</td>";
+                    echo "  <td>".$device[AppUpdater::DEVICE_PLATFORM]."</td>";
+                    echo "  <td>".$device[AppUpdater::DEVICE_LASTCHECK]."</td>";
                     echo "</tr>";
                 endforeach;
             ?>
