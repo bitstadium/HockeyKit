87d3334dd9b38e6579647fdfed4d34aa7c44a97b
Thomas Dohmke <thomas@dohmke.de>
2011-01-27 23:42:24 +0100

    Added demo project and README for Android.

---
+++ demo/Android/src/net/hockeyapp/android/demo/MainActivity.java
@@ -0,0 +1,34 @@
+package net.hockeyapp.android.demo;
+
+import net.hockeyapp.android.CheckUpdateTask;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+  private CheckUpdateTask checkUpdateTask;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    
+    checkForUpdates();
+  }
+
+  private void checkForUpdates() {
+    checkUpdateTask = (CheckUpdateTask)getLastNonConfigurationInstance();
+    if (checkUpdateTask != null) {
+      checkUpdateTask.attach(this);
+    }
+    else {
+      checkUpdateTask = new CheckUpdateTask(this, "http://worldviewmobileapp.com/apps/demo/");
+      checkUpdateTask.execute();
+    }
+  }
+
+  @Override
+  public Object onRetainNonConfigurationInstance() {
+    checkUpdateTask.detach();
+    return checkUpdateTask;
+  }
+}
