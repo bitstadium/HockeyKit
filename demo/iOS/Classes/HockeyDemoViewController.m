f17b79504229221dd399f4df4f7f7e45464f8bce
Peter Steinberger <me@petersteinberger.com>
2011-02-09 18:42:35 +0100

    demo is now landscape-able

--- demo/iOS/Classes/HockeyDemoViewController.m
+++ demo/iOS/Classes/HockeyDemoViewController.m
@@ -20,47 +20,51 @@
 
 - (void)openUpdateViewAnimated:(BOOL)animated {
 #if !defined (CONFIGURATION_AppStore_Distribution)
-  BWHockeyViewController *hockeyViewController = [[BWHockeyManager sharedHockeyController] hockeyViewController:YES];
-  UINavigationController *hockeyNavController = [[[UINavigationController alloc] initWithRootViewController:hockeyViewController] autorelease];
-  IF_3_2_OR_GREATER(hockeyNavController.modalPresentationStyle = UIModalPresentationFormSheet;)
-  [self presentModalViewController:hockeyNavController animated:animated];
+    BWHockeyViewController *hockeyViewController = [[BWHockeyManager sharedHockeyController] hockeyViewController:YES];
+    UINavigationController *hockeyNavController = [[[UINavigationController alloc] initWithRootViewController:hockeyViewController] autorelease];
+    IF_3_2_OR_GREATER(hockeyNavController.modalPresentationStyle = UIModalPresentationFormSheet;)
+    [self presentModalViewController:hockeyNavController animated:animated];
 #endif
 }
 
 #ifdef kAutoOpenHockey
 - (void)openUpdateViewCaller {
-  [self openUpdateViewAnimated:NO];
+    [self openUpdateViewAnimated:NO];
 }
 #endif
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
-
-  self.navigationItem.leftBarButtonItem =  [[UIBarButtonItem alloc] initWithTitle:@"Settings"
-                                                                            style:UIBarButtonItemStyleBordered
-                                                                           target:self
-                                                                           action:@selector(showSettings)];
-
+    [super viewDidLoad];
+    
+    self.navigationItem.leftBarButtonItem =  [[UIBarButtonItem alloc] initWithTitle:@"Settings"
+                                                                              style:UIBarButtonItemStyleBordered
+                                                                             target:self
+                                                                             action:@selector(showSettings)];
+    
 #ifdef kAutoOpenHockey
-  // HACK - DEVELOPMENT AID
-  [self performSelector:@selector(openUpdateViewCaller) withObject:nil afterDelay:0.01];
+    // HACK - DEVELOPMENT AID
+    [self performSelector:@selector(openUpdateViewCaller) withObject:nil afterDelay:0.01];
 #endif
 }
 
 - (void)dealloc {
-  [super dealloc];
+    [super dealloc];
 }
 
 
 - (void)showSettings {
-  HockeyDemoSettingsViewController *hockeySettingsViewController = [[[HockeyDemoSettingsViewController alloc] init] autorelease];
-  UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:hockeySettingsViewController];
-  navController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
-  [self presentModalViewController:navController animated:YES];
+    HockeyDemoSettingsViewController *hockeySettingsViewController = [[[HockeyDemoSettingsViewController alloc] init] autorelease];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:hockeySettingsViewController];
+    navController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    [self presentModalViewController:navController animated:YES];
 }
 
 - (IBAction)openUpdateView {
-  [self openUpdateViewAnimated:YES];
+    [self openUpdateViewAnimated:YES];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    return toInterfaceOrientation != UIInterfaceOrientationPortraitUpsideDown;
 }
 
 @end
