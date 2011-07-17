776b4f03e843ec030d9a60bde6aa21bdd07dd0d0
Peter Steinberger <me@petersteinberger.com>
2011-07-17 20:33:06 +0200

    add conditionals for iOS5

--- client/iOS/BWGlobal.h
+++ client/iOS/BWGlobal.h
@@ -98,3 +98,24 @@ if (kCFCoreFoundationVersionNumber < kCFCoreFoundationVersionNumber_iPhoneOS_4_0
 { \
 __VA_ARGS__ \
 }
+
+
+
+#ifndef kCFCoreFoundationVersionNumber_iPhoneOS_5_0
+#define kCFCoreFoundationVersionNumber_iPhoneOS_5_0 666.1
+#endif
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 50000
+#define IF_IOS5_OR_GREATER(...) \
+if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iPhoneOS_5_0) \
+{ \
+__VA_ARGS__ \
+}
+#else
+#define IF_IOS5_OR_GREATER(...)
+#endif
+
+#define IF_PRE_IOS5(...)  \
+if (kCFCoreFoundationVersionNumber < kCFCoreFoundationVersionNumber_iPhoneOS_5_0)  \
+{ \
+__VA_ARGS__ \
+}
