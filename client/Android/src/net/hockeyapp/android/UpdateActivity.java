6349fc3116ce044a34674ecb5aa26acc955fd8eb
Thomas Dohmke <thomas@dohmke.de>
2011-11-03 17:39:28 +0100

    Removed warning.

--- client/Android/src/net/hockeyapp/android/UpdateActivity.java
+++ client/Android/src/net/hockeyapp/android/UpdateActivity.java
@@ -25,7 +25,6 @@ import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
