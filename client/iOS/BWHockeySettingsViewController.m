68ac6559d146b53e7ce580b86e469a24bb8e7122
Afonso Cavaco Neto <afonso.neto@inm.pt>
2015-07-23 18:47:57 +0100

    Fix analyze warning
    Fixed warning: The 'viewDidUnload' instance method in UIViewController
    subclass 'BWHockeySettingsViewController' is missing a [super
    viewDidUnload] call

--- client/iOS/BWHockeySettingsViewController.m
+++ client/iOS/BWHockeySettingsViewController.m
@@ -264,6 +264,7 @@
 }
 
 - (void)viewDidUnload {
+    [super viewDidUnload];
   // Relinquish ownership of anything that can be recreated in viewDidLoad or on demand.
   // For example: self.myOutlet = nil;
 }
